7598d9803e782f4c5d8a3a9e0b489c41f5605ee2
bmedmilbe <bm.edmilbe@gmail.com>
2023-08-15 15:31:46 +0100

    Set mail to other user

--- core/models.py
+++ core/models.py
@@ -8,6 +8,6 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255, null=True)
     code = models.CharField(max_length=255, null=True)
-    # USERNAME_FIELD = "email"
+    USERNAME_FIELD = "email"
     # REQUIRED_FIELDS = ["username"]  # removes email from REQUIRED_FIELDS
-    # REQUIRED_FIELDS = []  # removes email from REQUIRED_FIELDS
+    REQUIRED_FIELDS = []  # removes email from REQUIRED_FIELDS
