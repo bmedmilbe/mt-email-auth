d09c8b6f585951bb31dc59b0904b26a6c135b2e1
bmedmilbe <bm.edmilbe@gmail.com>
2023-08-15 15:24:24 +0100

    Fix set core

--- core/models.py
+++ core/models.py
@@ -9,5 +9,5 @@ class User(AbstractUser):
     phone = models.CharField(max_length=255, null=True)
     code = models.CharField(max_length=255, null=True)
     # USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username"]  # removes email from REQUIRED_FIELDS
+    # REQUIRED_FIELDS = ["username"]  # removes email from REQUIRED_FIELDS
     # REQUIRED_FIELDS = []  # removes email from REQUIRED_FIELDS
