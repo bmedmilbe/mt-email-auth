a7223cc673975faa246a6f64fc6c173668b4f948
bmedmilbe <bm.edmilbe@gmail.com>
2023-11-04 02:07:59 +0000

    Alter new vauesh

--- certificates/migrations/00040_alter_certificatesimpleperson_gender.py
+++ certificates/migrations/00040_alter_certificatesimpleperson_gender.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('certificates', '0001_initial'),
+        ('certificates', '00040_alter_certificatesimpleperson_gender'),
     ]
 
     operations = [
