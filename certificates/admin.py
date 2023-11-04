42fd0910ed2f9b499dd15f45986a3bba51c7f4d0
bmedmilbe <bm.edmilbe@gmail.com>
2023-11-04 21:11:01 +0000

    Start to inseri files

--- certificates/admin.py
+++ certificates/admin.py
@@ -272,10 +272,10 @@ class CertificateTitleAdmin(admin.ModelAdmin):
 
 @admin.register(models.Certificate)
 class CertificateAdmin(admin.ModelAdmin):
-    # def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
-    #     certificates = models.Certificate.objects.order_by("id")
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        certificates = models.Certificate.objects.order_by("id")
 
-    #     for certificate in certificates:
+        for certificate in certificates:
 
     #         if certificate.atestado_state == 1:
     #             certificate.status = "P"
@@ -291,19 +291,19 @@ class CertificateAdmin(admin.ModelAdmin):
     #         certificate.save()
 
     #     # # # # pprint(certificate)
-    #     # # file_path = certificate.number
-    #     #     file_path = f"/certificates/{certificate.type.certificate_type.id}/{certificate.type.id}/{certificate.number}.pdf"
-    #     #     folder_online = f"{certificate.type.id}-{certificate.type.certificate_type.slug}-de-{certificate.type.slug}/{certificate.number}.pdf"
+            file_path = certificate.number
+            file_path = f"/certificates/{certificate.type.certificate_type.id}/{certificate.type.id}/{certificate.number}.pdf"
+            folder_online = f"{certificate.type.id}-{certificate.type.certificate_type.slug}-de-{certificate.type.slug}/{certificate.number}.pdf"
 
-    #     #     if os.path.exists(str(settings.MEDIA_ROOT) + f"{file_path}"):
-    #     #         with open(str(settings.MEDIA_ROOT) + f"{file_path}", 'rb') as existing_file:
-    #     #             pprint(certificate.id)
-    #     #             # pprint(certificate.type.certificate_type.slug)
-    #     #             # certificate.file.save(f'{folder_online}', existing_file)
-    #     #     else:
-    #     #         pprint(str(settings.MEDIA_ROOT) + f"{file_path}")
+            if os.path.exists(str(settings.MEDIA_ROOT) + f"{file_path}"):
+                with open(str(settings.MEDIA_ROOT) + f"{file_path}", 'rb') as existing_file:
+                    pprint(certificate.id)
+                    # pprint(certificate.type.certificate_type.slug)
+                    certificate.file.save(f'{folder_online}', existing_file)
+            else:
+                pprint(str(settings.MEDIA_ROOT) + f"{file_path}")
 
-    #     return super().get_queryset(request)
+        return super().get_queryset(request)
 
     list_display = [
         "type", "number", "text", "main_person", "secondary_person", "date_issue"
