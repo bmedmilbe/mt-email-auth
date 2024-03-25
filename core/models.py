060c578fc0459aecfaacb52ef0c85c2ebae710aa
bmedmilbe <bm.edmilbe@gmail.com>
2024-03-25 11:53:34 +0000

    Set phone to blank

--- core/models.py
+++ core/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=255, null=True)
+    phone = models.CharField(max_length=255, null=True, blank=True)
     valid = models.BooleanField(default=False)
 
     parthner = models.IntegerField(default=1, null=True)
