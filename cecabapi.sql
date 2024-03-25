6f8e6ce018ee6a2260f3106c42c45cd8a547ba3a
bmedmilbe <bm.edmilbe@gmail.com>
2024-03-25 12:40:44 +0000

    Remove sqls

--- cecabapi.sql
+++
@@ -1,9 +0,0 @@
---
--- Data for Name: cecab_association; Type: TABLE DATA; Schema: public; Owner: mivtooimwtygwz
---
-
-
-COPY "public"."core_user" ("id", "password", "last_login", "is_superuser", "username", "first_name", "last_name", "is_staff", "is_active", "date_joined", "phone", "email", "valid") FROM stdin;
-23	pbkdf2_sha256$600000$IZjePWfdOHs9cqFvee72bd$v6jH2J6v03QB59zpq3wiMoNOfOkC+CxRR9sJaD22Zj4=	\N	f	adias.cecab@gmail.com	Antonio	Dias	t	t	2023-11-24 15:08:41+00	8758654	adias.cecab@gmail.com	f
-24	pbkdf2_sha256$600000$ElMfpGOzCLfwnfer9Fdv5T$x0U8HqKVRv5rREVD3/b7TH24pMfYlZJgLP4XJQc/3AI=	2024-02-20 09:56:11.679573+00	f	direcaocecab@gmail.com	Luis	Cuba	t	t	2023-11-24 10:31:20+00	999999	direcaocecab@gmail.com	f
-...
