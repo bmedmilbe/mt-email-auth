8e0d9cfbcece1cdf7817e20e96e9964e8a11f8b6
bmedmilbe <bm.edmilbe@gmail.com>
2024-03-25 11:21:55 +0000

    Add phone to users

--- core/models.py
+++ core/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255, null=True)
     parthner = models.IntegerField(default=1)
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]  # removes email from REQUIRED_FIELDS
