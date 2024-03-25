af6dfd106d7e3e1c6c415e56453923e18b609667
bmedmilbe <bm.edmilbe@gmail.com>
2024-03-25 11:45:54 +0000

    Change id 9 to 10

--- ormedstpapi.sql
+++ ormedstpapi.sql
@@ -5,7 +5,7 @@
 
 COPY "public"."core_user" ("id", "password", "last_login", "is_superuser", "username", "first_name", "last_name", "is_staff", "is_active", "date_joined", "phone", "email", "valid") FROM stdin;
 22	pbkdf2_sha256$600000$WBBF8vxh80GqO1j5BHJi2V$l4jfbhmDjPc/aNuVaeL500BMrmXNtcxhtsM49omTM2k=	\N	f	aguilardominguez87@gmail.com	Yelena	Barroso	f	t	2023-07-21 12:22:28.308511+00	\N	aguilardominguez87@gmail.com	f
-9	pbkdf2_sha256$600000$bHTN3Cg997NUn3qEDCDvXV$WkziTBJDbqxf6/YOBR4TyaUfbgXIoB6hFrPGjdrhTEM=	\N	f	cremildebraganca@hotmail.com	Cremilde	Bragança	f	t	2023-07-21 12:25:33.482331+00	\N	cremildebraganca@hotmail.com	f
+10	pbkdf2_sha256$600000$bHTN3Cg997NUn3qEDCDvXV$WkziTBJDbqxf6/YOBR4TyaUfbgXIoB6hFrPGjdrhTEM=	\N	f	cremildebraganca@hotmail.com	Cremilde	Bragança	f	t	2023-07-21 12:25:33.482331+00	\N	cremildebraganca@hotmail.com	f
 21	pbkdf2_sha256$600000$uCMcNz6WpNWqwBGhqOIAH3$PgbywCpvYdP3eFfsiMn5CjqSxY8CS6YbVo5i48yUbrI=	\N	t	m.cassandra.soares@gmail.com	Miryan	Bandeira dos Prazeres Cassandra Soares	t	t	2023-07-20 13:42:00+00	9913920	m.cassandra.soares@gmail.com	t
 15	pbkdf2_sha256$600000$chsCO0YHrlzPtxsTlTvaDP$aqzxA0B+xCmx+5XXqH9uTH0quM4xMwvr/17q3GuOM5w=	2023-12-09 05:56:40.220431+00	t	edmilbe	Edmilbe	Ramos	t	t	2023-04-12 16:11:43+00	07448019595	edmilbe@gmail.com	f
 11	pbkdf2_sha256$600000$octow83aVZlwFNU70tA6H6$F5MGnXHnmptdybk/FgYQ2E8bHlL26YqMhqpoks1Y4sg=	\N	f	niurkasolange@gmail.com	Niurka Solange	Silveira D’Almeida	f	t	2023-09-14 06:58:30.650852+00	\N	niurkasolange@gmail.com	f
@@ -24,7 +24,7 @@ COPY "public"."ormed_doctor" ("id", "birth_date", "bio", "id_number", "area_id",
 2	2005-05-31	hgfaduh ljhdlh	8748654	1	1	1	1	f	2023-06-27 09:26:55.372738+00	17	2023-06-27	\N	\N
 6	1983-03-31	Médica cardiologista, HAM	84608	4	1	1	1	t	2023-07-20 13:42:01.082166+00	21	2023-09-20		ormed/doctors/images/Screenshot_2023-07-09-20-31-15-743-edit_com.facebook.katana2.jpg
 4	1965-02-22	Licenciada em medicina desde 1985. Trabalho atualmente no serviço de pediatria do HAM	33713	1	1	1	1	t	2023-07-19 17:50:53.549846+00	19	2023-07-19		ormed/doctors/images/image0_2.jpeg
-8	1986-04-24	Ginecologista	93865	5	1	1	1	t	2023-07-21 12:25:33.695368+00	9	2026-06-06		
+8	1986-04-24	Ginecologista	93865	5	1	1	1	t	2023-07-21 12:25:33.695368+00	10	2026-06-06		
 3	1986-02-12	Yell Eric Jordão da Costa, nasceu no dia 12 de fevereiro de 1986 o distrito de Lobata em São Tomé.\r\nFilho de Antero Fernandes Nascimento da Costa e Augusta do Espírito Santo Jordão, viveu a sua infância na cidade Trindade com sua mãe mais duas irmãs. Frequentou o ensino primário, secundário  na Trindade, pré universitário no liceu Nacional, com término em 2004.	90443	1	1	1	1	t	2023-07-14 11:17:30.279806+00	18	2026-10-10	ormed/doctors/ids/IMG_20230624_062931.jpg	ormed/doctors/images/IMG_20220731_193723.jpg
 7	1987-01-20	Médica especialista en Ortopedia	232311	2	1	1	1	t	2023-07-21 12:22:28.511439+00	22	2025-11-29		
 1	1994-04-19	Médico de Clinica Geral formado em Cuba na Escola Latinoamericana de Medicina.	116039	1	1	1	1	t	2023-06-22 08:42:11.23482+00	16	2023-09-06		ormed/doctors/images/image.jpg
