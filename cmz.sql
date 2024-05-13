9d8a04abbb6d4bac118b10969432237f1a0c759f
bmedmilbe <bm.edmilbe@gmail.com>
2024-05-13 01:57:37 +0100

    Add 2 lines on end

--- cmz.sql
+++ cmz.sql
@@ -1,23 +1,7 @@
 
-
-
-COPY "public"."cmz_imagestour" ("id", "image", "tour_id") FROM stdin;
-2	camaramz/tour/images/Roça_Monte_Café_-_São_Tomé_e_Príncipe_Foto_de_Avlis_Rotieh.jpg	2
-3	camaramz/tour/images/Captura_de_ecrã_2020-03-14_às_14.22.00.png	2
-4	camaramz/tour/images/roc3a7a-monte-cafc3a9-distrito-de-mc3a9-zoxi-ilha-de-sc3a3o-tomc3a9.jpg	2
-5	camaramz/tour/images/Roca-Bombaim.Sao-Tome.www_.saotomechoice.com_.jpg	3
-6	camaramz/tour/images/bombaim02.jpg	3
-7	camaramz/tour/images/sao-tome-2022-0159-1.jpg	4
-8	camaramz/tour/images/download.jpeg	4
-9	camaramz/tour/images/pico-de-sao-tome.jpg	4
-\.
-
---
--- Data for Name: cmz_service; Type: TABLE DATA; Schema: public; Owner: eurrkvbyncwetd
---
-
 COPY "public"."cmz_service" ("id", "name", "slug", "picture", "description") FROM stdin;
 2	Atestados	atestados	camaramz/services/images/certificates.jpg	Nós atestamos junto a outra organizações sobre o teu estado dentro do Distrito de Mé-Zóchi
 3	Entrega de Caixote de Lixo	entrega-de-caixote-de-lixo	camaramz/services/images/pawel-czerwinski-RkIsyD_AVvc-unsplash.jpg	Através desse website podes mandar uma mensagem e soliciitar o seu caixote de lixo.
 1	Autorizações e Licenças	autorizacoes-e-licencas	camaramz/services/images/jana-shnipelson-iC3z2DkVcdE-unsplash.jpg	Pode pedir autorização para desencadear diferentes acções dentro do Distrito de Mé-Zóchi.
 \.
+
