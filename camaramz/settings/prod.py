9088bd558060f9e8cd386ad4cf9571cf5b0d8132
bmedmilbe <bm.edmilbe@gmail.com>
2025-01-07 11:22:03 +0000

    Static files production

--- camaramz/settings/prod.py
+++ camaramz/settings/prod.py
@@ -72,6 +72,8 @@ AWS_SECRET_ACCESS_KEY = 'c2qIEzuaRKArLjVxQUa5LrDLy98k4NpOQFupfE8Y'
 AWS_STORAGE_BUCKET_NAME = 'bm-edmilbe-bucket'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # storages.backends.s3boto3.S3Boto3Storage
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
