687d98935ecbbe43661eb98184fdac34e9cef95a
bmedmilbe <bm.edmilbe@gmail.com>
2025-01-20 11:46:28 +0000

    Files

--- camaramz/settings/prod.py
+++ camaramz/settings/prod.py
@@ -70,7 +70,7 @@ EMAIL_USE_TLS = True
 AWS_ACCESS_KEY_ID = 'AKIA2QHYRZDKXSR7F2KK'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'bm-edmilbe-bucket'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # storages.backends.s3boto3.S3Boto3Storage
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
