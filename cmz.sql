ab78d401bc54b119cbd283b1272f3bcf8c678009
bmedmilbe <bm.edmilbe@gmail.com>
2026-01-07 06:40:30 +0000

    Remove all unused from root

--- cmz.sql
+++
@@ -1,7 +0,0 @@
-
-COPY "public"."cmz_service" ("id", "name", "slug", "picture", "description") FROM stdin;
-2	Atestados	atestados	camaramz/services/images/certificates.jpg	Nós atestamos junto a outra organizações sobre o teu estado dentro do Distrito de Mé-Zóchi
-3	Entrega de Caixote de Lixo	entrega-de-caixote-de-lixo	camaramz/services/images/pawel-czerwinski-RkIsyD_AVvc-unsplash.jpg	Através desse website podes mandar uma mensagem e soliciitar o seu caixote de lixo.
-1	Autorizações e Licenças	autorizacoes-e-licencas	camaramz/services/images/jana-shnipelson-iC3z2DkVcdE-unsplash.jpg	Pode pedir autorização para desencadear diferentes acções dentro do Distrito de Mé-Zóchi.
-\.
-
