ce8591a8ec4fa73e4356d02eb684e1e10e36d9b6
bmedmilbe <bm.edmilbe@gmail.com>
2026-01-11 13:20:08 +0000

    Add File for Production

--- camaramz/settings/prod.py
+++ camaramz/settings/prod.py
@@ -7,6 +7,8 @@ DEBUG = False
 SECRET_KEY = os.environ["SECRET_KEY"]
 ALLOWED_HOSTS = [".railway.app", ".up.railway.app"]
 
+DJANGO_SETTINGS_MODULE=os.environ["DJANGO_SETTINGS_MODULE"]
+
 # --- CORS & CSRF CONFIGURATION ---
 CORS_ALLOW_ALL_ORIGINS = False
 
