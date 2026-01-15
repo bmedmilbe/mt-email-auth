09603c8b6df4001b5face861af7a2a02bf130588
bmedmilbe <bm.edmilbe@gmail.com>
2026-01-15 05:26:32 +0000

    refactor: migrate metadata engine to core app for centralized orchestration
    - Moved metadata aggregation logic from certificates to core
    - Utilized cross-app imports to decouple business data from core service delivery
    - Updated routing to provide a single /core/metadata entry point for all frontends

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,7 @@
-from django.views.generic import TemplateView
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
-    # path('', TemplateView.as_view(template_name='core/index.html'))
-    # path('password/', views.PasswordViewSet, basename="password"),
-
-    path('core/email', views.say_hello, name="email"),
-    # path('core/myaddress', views.my_address, name="my-address"),
-    path('core/myaddress', views.my_address, name="my-address"),
-    
+    path('metadata', views.UnifiedMetadataView.as_view(), name="unified-metadata"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,69 +1,42 @@
-from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework.views import APIView
-from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
-from templated_mail.mail import BaseEmailMessage
-import requests
-import json
-from pprint import pprint
-# Create your views here.
-import logging
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import action, api_view
-from rest_framework.response import Response
-from rest_framework import serializers, status
-from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from certificates.models import (
+    Country, IDType, Instituition, Street, Town, County, 
+    University, Ifen, BiuldingType, Cemiterio, Change, CertificateTitle, Coval
+)
+from certificates.serializers import MetadataSerializer
 
-logger = logging.getLogger(__name__)
-
-
-# class HelloView(APIView):)
-
-def say_hello(request):
-    try:
-        # send_mail('subject', 'message', 'edmilbe@gmail.com', ['bob@moshbuy.com'])
-        # mail_admins('subject', 'message', html_message='message')
-        # message = EmailMessage('subject', 'message', 'edmilbe@gmail.com', ['bob@moshbuy.com'])
-        # message.attach_file("core/static/images/contact-us.png")
-        # message.send()
-
-        message = BaseEmailMessage(
-            template_name='emails/hello.html',
-            context={'name': 'Mosh Hamedanny'})
-        message.attach_file("core/static/images/contact-us.png")
-        message.send(['john@bonsmabos.com'])
-
-    except BadHeaderError:
-        pass
-
-    return render(request, 'core/hello.html', {'msg': 'Email was successfully sent!'})
-
-
-
-@api_view(['GET'])
-def my_address(request):
+class UnifiedMetadataView(APIView):
     """
-    List all code snippets, or create a new snippet.
+    Centralized metadata provider. 
+    Optimized with select_related to prevent N+1 query overhead.
     """
+    def get(self, request):
+        data = {
+            "countries": Country.objects.all().order_by('name'),
+            "universities": University.objects.all().order_by('name'),
+            "ifens": Ifen.objects.all().order_by('name'),
+            "buildings": BiuldingType.objects.all().order_by('name'),
+            "cemiterios": Cemiterio.objects.all().order_by('name'),
+            "streets": Street.objects.select_related('town').all().order_by('name'),
+            "changes": Change.objects.all().order_by('name'),
+            "towns": Town.objects.select_related('county').all().order_by('name'),
+            "countys": County.objects.all().order_by('name'),
+            "certificateTitles": CertificateTitle.objects.all().order_by('name'),
+            "covals": Coval.objects.all().order_by('number'),
+            "idtypes": IDType.objects.all().order_by('name'),
+            "intituitions": Instituition.objects.all().order_by('name'),
+        }
+        serializer = MetadataSerializer(data)
+        return Response(serializer.data)
 
-    headers = {'Accept': 'application/json'}
 
-    ip = requests.get('http://api.ipify.org?format=json', headers=headers)
-   
-    ip_data = json.loads(ip.text)
-    res = requests.get('http://ip-api.com/'+ip_data['ip'], headers=headers)
 
-    post_code = ""
-    location_data_as = res.text.strip().split('"')
-    for i in range(len(location_data_as)):
-        if location_data_as[i] == 'zip': 
-            post_code = location_data_as[i+2]
-    
-    return Response({'data':post_code})
+
+
+
    
     
 
-    # return render(request, 'core/hello.html', {'data':location_data})
 
