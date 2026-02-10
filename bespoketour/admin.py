54df9551f3a99e4f2b15f3be8bcd2c800c601c6b
bmedmilbe <bm.edmilbe@gmail.com>
2026-02-10 12:25:03 +0000

    Setted Admin for Bespoke Tour

--- bespoketour/admin.py
+++ bespoketour/admin.py
@@ -1,9 +1,39 @@
 from django.contrib import admin
 
-from bespoketour.models import Customer
+from bespoketour.models import BespokeTag, Customer, ProfileTag, ProfileType
+
+
+
+class ProfileTagInline(admin.TabularInline):
+    model = ProfileTag
+    
 
 # Register your models here.
+class GeneralAdmin(admin.ModelAdmin):
+    list_per_page = 20
+
+@admin.register(ProfileType, BespokeTag)
+class ProfileAndTagAdmin(admin.ModelAdmin):
+    list_display = ["title", 'image']
+    ordering = ["title"]
+    list_editable = ["image"]
+    
+    prepopulated_fields = {"slug": ("title",),"image": ("slug",)}
+    search_fields = ["title"]
+    inlines = [ProfileTagInline]
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ["id"]
+    list_display = ["user"]
+    ordering = ["user"]
+    autocomplete_fields = ["profile_type"]
+
+
+
+
+
+
+
+
+
+
--- bespoketour/models.py
+++ bespoketour/models.py
@@ -7,17 +7,21 @@ class ProfileType(models.Model):
     title = models.CharField(max_length=255, unique=True)
     slug = models.SlugField(max_length=255, unique=True)
     image=models.CharField(max_length=255, unique=True)
-
+    def __str__(self):
+        return self.title
 class BespokeTag(models.Model):
     title = models.CharField(max_length=255, unique=True)
     slug = models.CharField(max_length=255, unique=True)
     image=models.CharField(max_length=255, unique=True)
-
+    def __str__(self):
+        return self.title
 
 class ProfileTag(models.Model):
     profile_type = models.ForeignKey(ProfileType, on_delete=models.CASCADE)
     bespoke_tag = models.ForeignKey(BespokeTag, on_delete=models.CASCADE)
-
+    
+    class Meta():
+        unique_together = ["profile_type", "bespoke_tag"]
 
 class Customer(models.Model):
     user = models.OneToOneField(
